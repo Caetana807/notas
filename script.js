16d357461a1877de56a010bb742a9cb4cdebab88
Caetana807 <caetanamarchesi@gmail.com>
2025-10-22 17:23:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcular(){
+    let nota1= Number(document.getElementById("n1").value)
+    if (nota1 <0 || nota1 > 3){
+        alert("Insira uma nota válida entre 0 e 3")    
+        return;
+
+}
+let nota1= Number(document.getElementById("n1").value)
+    if (nota1 <0 || nota1 > 3){
+        alert("Insira uma nota válida entre 0 e 3")    
+        return;
+
+}
